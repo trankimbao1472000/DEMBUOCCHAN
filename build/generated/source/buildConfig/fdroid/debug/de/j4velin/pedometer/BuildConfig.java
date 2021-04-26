7d0a5e9e9be5920216d29a31e1cf598b10433da8
dtdat <tiendat201228@gmail.com>
2021-04-26 17:10:45 +0700

    26/4

---
+++ build/generated/source/buildConfig/fdroid/debug/de/j4velin/pedometer/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package de.j4velin.pedometer;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "de.j4velin.pedometer";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "fdroid";
+  public static final int VERSION_CODE = 1511;
+  public static final String VERSION_NAME = "1.5.11";
+}
